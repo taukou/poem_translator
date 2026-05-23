a414b036e6438ee798a766b30072f77cd0107601
Thomas-2032 <s112403015@g.ncu.edu.tw>
2026-05-24 00:28:34 +0800

    剛不知道出了啥錯，重新commit 一次

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> backup/frontend-before-push
 from .translator import translate_poem, classical_to_modern_chinese, azure_translate
 from .emotion_analyzer import EmotionAnalyzer
 
 __all__ = [
     'translate_poem',
-    'classical_to_modern_chinese', 
+    'classical_to_modern_chinese',
     'azure_translate',
     'EmotionAnalyzer'
 ]
